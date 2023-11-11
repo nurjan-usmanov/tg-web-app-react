c329e8e2204501d05b00b70c4fa39d129df42212
nurjan-usmanov <nurjanuk@gmail.com>
2023-11-12 01:03:46 +0500

    init

--- src/App.js
+++ src/App.js
@@ -3,22 +3,25 @@ import { useEffect } from 'react';
 import './App.css';
 const tg = window.Telegram.WebApp;
 
+// create the App component
 function App() {
 
+  // on mount, initialize the tg bot
   useEffect(()=>{
-    tg.ready();
+     tg.ready();
   }, [])
-
+ 
+  // close the tg bot when button clicked
   const onClose = () => {
-    tg.close()
+     tg.close()
   }
-
+ 
   return (
-    <div className="App">
-      Hello
-      <button onClick={onClose}>Закрыть</button>
-    </div>
+     <div className="App">
+       Hello
+       <button onClick={onClose}>Закрыть</button>
+     </div>
   );
-}
+ }
 
 export default App;
